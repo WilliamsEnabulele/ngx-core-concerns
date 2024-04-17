198e4924d0f3060df1fa0428b33b11d6c7af47f8
Williams Enabulele <enabsdigital@gmail.com>
2024-04-17 21:41:52 +0100

    chore: update public-api

--- projects/ngx-essentials/src/public-api.ts
+++ projects/ngx-essentials/src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of ngx-essentials
  */
 
-export * from './lib/ngx-essentials.service';
-export * from './lib/ngx-essentials.component';
+export * from './lib/ngx-validations.service';
+export * from './lib/ngx-validation/ngx-validation.module';
+export * from './lib/ngx-validation/ngx-validation-message/ngx-validation-message.component';
